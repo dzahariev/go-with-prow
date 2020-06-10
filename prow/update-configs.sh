c561021dc9494e8dfc98ac672bf30b2164874392
Dobromir Zahariev <dobromir.zahariev@sap.com>
2020-06-10 18:53:06 +0300

    Initial commit

---
+++ prow/update-configs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x 
+
+kubectl create configmap plugins \
+  --from-file=plugins.yaml=plugins.yaml --dry-run=client -o yaml | kubectl replace configmap plugins -f -
+
+kubectl create configmap config \
+  --from-file=config.yaml=config.yaml --dry-run=client -o yaml | kubectl replace configmap config -f -
