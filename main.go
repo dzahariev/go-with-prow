314abf524db821bc5e8a2de18e2defa1f7340e9c
Dobromir Zahariev <dobromir.zahariev@sap.com>
2020-06-17 22:07:26 +0300

    Remove unused packages

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"dzahariev/go-with-prow/pkg01"
-	"dzahariev/go-with-prow/pkg02"
 	"fmt"
 )
 
 func main() {
 	fmt.Println(hello())
-	fmt.Println(pkg01.HelloPkg01())
-	fmt.Println(pkg02.HelloPkg02())
 }
 
 func hello() string {
